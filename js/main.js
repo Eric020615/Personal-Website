175ea2e46a66591e077420a294849de542307a62
ericj <ericjuncheng10@gmail.com>
2023-03-05 22:17:25 +0800

    testing

--- js/main.js
+++ js/main.js
@@ -190,6 +190,20 @@ function scrollActive(){
         }
     })
 }
+
 window.addEventListener('scroll',scrollActive)
 
 // change background header
+
+function scrollHeader(){
+    // to link the header to the variable nav
+    const nav = document.getElementById('header')
+    if(this.scrollY >= 80){
+        nav.classList.add('scroll-header')
+    }
+    else{
+        nav.classList.remove('scroll-header')
+    }
+}
+
+window.addEventListener('scroll',scrollHeader)
